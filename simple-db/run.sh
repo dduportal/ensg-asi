9c061bf34c0f4802472bdf025a42ec2c91a67f3a
Damien DUPORTAL <damien.duportal@gmail.com>
2014-11-16 14:45:40 +0100

    Simple DB : adding more delay beetween srv and client

--- simple-db/run.sh
+++ simple-db/run.sh
@@ -12,7 +12,7 @@ docker rm $(docker ps -a -q)
 
 echo "== Launching postgreSQL image and waiting 2 seconds ..."
 docker run --name ${SERVER_NAME} -d ${IMAGE_NAME}
-sleep 2
+sleep 5
 
 echo "== Running the sql script to create all objects"
 docker run --link ${SERVER_NAME}:db -ti -v /vagrant:/vagrant ${IMAGE_NAME} sh -c 'exec psql -h "$DB_PORT_5432_TCP_ADDR" -p "$DB_PORT_5432_TCP_PORT" -U postgres -f /vagrant/simple-db/comptes.sql'
